de332ecb5e0fab66e3bcd39d4dbc04ff4aa61f70
Napadon <napadonnantasuk@gmail.com>
2025-09-11 23:11:47 +0700

    fix: add ridesRoutes and requestRoutes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import "dotenv/config";
 import express from "express";
 import { logger } from "./utils/logger";
 import { requestLog } from "./middlewares/logging";
+import ridesRoutes from './routes/ridesRoutes';
+import requestRoutes from "./routes/requestRoutes";
 
 export const app = express();
 
+app.use(express.json());
 app.use(requestLog);
+
+app.use('/api/rides', ridesRoutes);
+app.use("/api/requests", requestRoutes);
 
 const port = +(process.env.PORT || 8000);
 app.listen(port);
