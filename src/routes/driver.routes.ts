422620524c042a4e695b9cde92bc4b7847a566ae
Krissada Singhakachain <omsin_47@hotmail.com>
2025-09-15 14:00:32 +0700

    fix: added auth middleware to driver routes

--- src/routes/driver.routes.ts
+++ src/routes/driver.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { updateDriverLocation, getDrivers } from "../controllers/driver.controller";
+import {
+	updateDriverLocation,
+	getDrivers,
+} from "../controllers/driver.controller";
+import { auth } from "../middlewares/auth";
 
 const router = Router();
 
 // Driver updates location
-router.post("/location", updateDriverLocation);
+router.post("/location", auth, updateDriverLocation);
 
 // Customer fetches all drivers
-router.get("/location", getDrivers);
+router.get("/location", auth, getDrivers);
 
 export default router;
