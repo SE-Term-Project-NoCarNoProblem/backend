1b0702bf0e0db738214f42739e080d8ab75c8346
Akaporn <150357038+Akaporn@users.noreply.github.com>
2025-10-19 19:54:50 +0700

    fix: delete dist

--- dist/middlewares/logging.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestLog = requestLog;
-const logger_1 = require("../utils/logger");
-function requestLog(req, _res, next) {
-    logger_1.logger.http(`${req.method} ${req.path}`);
-    next();
-}
--- dist/routes/ride.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ride_controller_1 = require("../controllers/ride.controller");
-const auth_1 = require("../middlewares/auth");
-const router = (0, express_1.Router)();
-router.post('/accept/', auth_1.auth, ride_controller_1.acceptRide);
-router.delete('/cancel/:id', auth_1.auth, ride_controller_1.cancelRide);
-exports.default = router;
