27343888c100b05431cb1e11b18edb060820e5ba
Thitiwut-Win <bmwin9825@gmail.com>
2025-11-03 15:56:56 +0700

    Update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -12,6 +12,6 @@ const router = Router();
 router.get("/me", auth, getMe);
 router.patch("/me", auth, patchMe);
 router.get("/:id", getUser);
-router.patch(":/id/status", updateUserStatus);
+router.patch(":/id/status", auth, updateUserStatus);
 
 export default router;
