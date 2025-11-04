fdd714d3e009f1577f9459ef57c58269fbeaecc8
Krissada Singhakachain <omsin_47@hotmail.com>
2025-11-04 20:20:17 +0700

    refactor: adjust db to only allow one resolver on a support ticket

--- prisma/migrations/add_ride_progress_status.sql
+++
@@ -1,5 +0,0 @@
--- Create the enum type for ride progress status
-CREATE TYPE ride_progress_status AS ENUM ('accepted', 'on_the_way', 'arrived', 'picked_up', 'completed');
-
--- Add the column to the ride table with a default value
-ALTER TABLE ride ADD COLUMN ride_progress_status ride_progress_status NOT NULL DEFAULT 'accepted';
