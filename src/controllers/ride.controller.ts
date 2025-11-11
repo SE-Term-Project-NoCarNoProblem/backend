a8ca294483bfa32a43dff06eff840e21810f0f61
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-11-11 11:53:31 +0700

    feat: implement middleware for restrict ride accept

--- src/controllers/ride.controller.ts
+++ src/controllers/ride.controller.ts
@@ -38,12 +38,7 @@ export async function acceptRide(req: Request, res: Response) {
 		const rideId = req.params.id;
 		if (!driverId)
 			return res.status(401).json({ error: "User not authenticated" });
-		const verified = await prisma.verified_driver.findUnique({
-			where: { id: driverId },
-		});
-		if (!verified) {
-			return res.status(403).json({ error: "Driver not verified" });
-		}
+
 		const rideReq = getRequest(rideId);
 		if (!rideReq) {
 			const wasAccepted = getAcceptedRequest(rideId);
